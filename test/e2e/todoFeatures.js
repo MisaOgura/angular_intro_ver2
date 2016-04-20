9df50aad3ec84e43eaf052e4ae42fad625ae8c97
Misa Ogura <misa.ogura01@gmail.com>
2016-04-20 14:32:04 +0100

    Install Karma and related packages. Create controller, first unit test passing

--- test/e2e/todoFeatures.js
+++ test/e2e/todoFeatures.js
@@ -1,6 +1,12 @@
 describe('Todos tracker', function() {
   it('has a title', function() {
-    browser.get('/');
+    browser.get('/app');
     expect(browser.getTitle()).toEqual('Todos App');
+  });
+
+  it('has a todo: Todo1', function() {
+    browser.get('/app');
+    var todo = $('#todo');
+    expect(todo.getText()).toContain('ToDo1');
   });
 });
